b7758eed5d057f9103e741f7ba705bc3bc84d4fd
IKERRIZ <okothfaith94@gmail.com>
2019-10-28 01:26:25 +0300

    Add user authentication

--- app/models.py
+++ app/models.py
@@ -1,11 +1,37 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
+
 class Quote:
     '''
     Quote class to define the quotes objects
     
     '''
 
-    def __init__(self,id,author,quote,quote_link):
-        self.id = id
+    def __init__(self,author,quote):
         self.author = author
         self.quote = quote
-        self.quote_link = "http://quotes.stormconsultancy.co.uk/quotes/25" 
+
+ class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+    hash_pass = db.Column(db.String(255))  
+
+     @property
+    def password(self):
+        raise AttributeError("You can not read password attribution")
+    @password.setter
+    def password(self, password):
+        self.hash_pass = generate_password_hash(password)
+
+    def set_password(self,password):
+        self.hash_pass = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.hash_pass, password)
+
+
+    def __repr__(self):
+        return f'User {self.username}'     
