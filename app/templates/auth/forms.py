9be51d6fb9cb3dfd0bd4681205a978007e3398fe
IKERRIZ <okothfaith94@gmail.com>
2019-10-28 02:38:16 +0300

    Create a class to login/out a user,resetpassword and change password

--- app/templates/auth/forms.py
+++ app/templates/auth/forms.py
@@ -20,3 +20,18 @@ class RegistrationForm(FlaskForm):
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address', validators=[Required(),Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class ResetPassword(FlaskForm):
+    email = StringField('Email', validators=[Required(), Email()])
+    submit = SubmitField('Reset Password')
+
+class NewPassword(FlaskForm):
+    password = PasswordField('Password',validators=[Required()])
+    password_repeat = PasswordField('Repeat Password', validators=[Required(),EqualTo('password')])
+    submit = SubmitField('Change Password')
