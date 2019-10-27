5491aa5b033f7248e7b9b97d40fbe077562f0841
IKERRIZ <okothfaith94@gmail.com>
2019-10-27 16:18:02 +0300

    Set configurationoptions

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,6 @@ from .config import DevConfig
 app = Flask(__name__)
 
 # Setting up configuration
-app.config.from_object(DevConfig)
+app.config.from_object(config_options[config_name]) 
 
 from app import views
