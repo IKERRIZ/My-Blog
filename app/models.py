13278abee8e510d0268c2338da00e359112ac24b
IKERRIZ <okothfaith94@gmail.com>
2019-10-28 02:16:58 +0300

    Setthe login manager

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from . import db
+from . import db, login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 
@@ -12,12 +12,18 @@ class Quote:
         self.author = author
         self.quote = quote
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+       
+
  class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(255))
     username = db.Column(db.String(255), unique = True, nullable = False)
+     email = db.Column(db.String(255), unique = True, nullable = False )
+    profile_pic_path = db.Column(db.String(255), nullable = False, default = 'avatar.jpg')
     hash_pass = db.Column(db.String(255))  
 
      @property
