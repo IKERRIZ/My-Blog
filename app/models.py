a4c8dffcd2686e2d5f06680d4f54fc0275979908
IKERRIZ <okothfaith94@gmail.com>
2019-10-28 02:25:19 +0300

    Add a post column in the users table

--- app/models.py
+++ app/models.py
@@ -22,9 +22,10 @@ def load_user(user_id):
 
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255), unique = True, nullable = False)
-     email = db.Column(db.String(255), unique = True, nullable = False )
+    email = db.Column(db.String(255), unique = True, nullable = False )
     profile_pic_path = db.Column(db.String(255), nullable = False, default = 'avatar.jpg')
     hash_pass = db.Column(db.String(255))  
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
      @property
     def password(self):
@@ -41,4 +42,11 @@ def load_user(user_id):
 
 
     def __repr__(self):
-        return f'User {self.username}'     
+        return f'User {self.username}'    
+
+ class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable = False)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
