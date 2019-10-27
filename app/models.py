ec8ceede6cebdb18c1d62aad563dfe02f3d89b20
IKERRIZ <okothfaith94@gmail.com>
2019-10-28 02:27:36 +0300

    Created a relationship with a class user

--- app/models.py
+++ app/models.py
@@ -12,10 +12,12 @@ class Quote:
         self.author = author
         self.quote = quote
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
        
+
 
  class User(UserMixin, db.Model):
     __tablename__ = 'users'
